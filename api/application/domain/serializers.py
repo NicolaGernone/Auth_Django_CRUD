5cc5a79baf04318fe70a4aa3c2e446f94bd3ce18
Nicola Gernone <nicola.gernone02@gmail.com>
2023-06-02 00:27:18 +0200

    feat: add user serializer
    to perfor action on the user profile

--- api/application/domain/serializers.py
+++ api/application/domain/serializers.py
@@ -1,7 +1,31 @@
 from rest_framework import serializers
-from .entities import Profile
+from .entities import Profile, User
+from ..services import ProfileServices  
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        ProfileServices.create_user_profile(user)
+        return user
+
+    def update(self, instance, validated_data):
+        ProfileServices.update_user(instance, **validated_data)
+        return instance
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['name', 'phone_number', 'past_address', 'current_address']
+
+    def update(self, instance, validated_data):
+        profile = super().update(instance, validated_data)
+        ProfileServices.save_user_profile(profile.user)
+        return profile
+
+    def delete(self, instance):
+        ProfileServices.delete_user(instance.user)
+        return instance
