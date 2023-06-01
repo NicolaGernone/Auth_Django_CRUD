d36fae6f8757befe0aecdc3cdc3ba7f9bfa9f82e
Nicola Gernone <nicola.gernone02@gmail.com>
2023-06-02 00:28:10 +0200

    feat: implement the views
    using modelviewset to avoid extra code

--- api/infrastructure/views.py
+++ api/infrastructure/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Profile
+from django.contrib.auth.models import User
+from api.application.domain.serializers import ProfileSerializer, UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
