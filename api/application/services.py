e3b9333307721325dfb52dad5d058322a5ddaa84
Nicola Gernone <nicola.gernone02@gmail.com>
2023-06-02 00:29:17 +0200

    feat: CRUD services
    basic CRUD services

---
+++ api/application/services.py
@@ -0,0 +1,26 @@
+from .domain.entities import Profile
+from django.contrib.auth import get_user_model
+
+
+class ProfileServices:
+    
+    User = get_user_model()
+    
+    @staticmethod
+    def create_user_profile(user):
+        Profile.objects.get_or_create(user=user)
+
+    @staticmethod
+    def save_user_profile(user):
+        user.profile.save()
+
+    @staticmethod
+    def delete_user(user):
+        user.delete()
+
+    @staticmethod
+    def update_user(user, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(user, attr, value)
+        user.save()
+        
