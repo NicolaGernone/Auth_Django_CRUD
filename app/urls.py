c19a530d12ac3c7240430b4d48fe208e03e102d2
Nicola Gernone <nicola.gernone02@gmail.com>
2023-06-02 00:56:33 +0200

    feat: urls implementation

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+    path("api/", include("api.urls", namespace="api")),
     path("admin/", admin.site.urls),
+    path('docs/', include_docs_urls(title='Login API CRUD')),
 ]
