14c811146106bf32dcc671ae494bf718d7a6460a
Nicola Gernone <nicola.gernone02@gmail.com>
2023-06-02 01:12:14 +0200

    test: add profile factory

--- tests/application/domain/factories.py
+++ tests/application/domain/factories.py
@@ -1,6 +1,8 @@
 import factory
 from django.contrib.auth import get_user_model
 
+from api.application.domain.entities import Profile
+
 User = get_user_model()
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -9,3 +11,14 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     username = factory.Sequence(lambda n: f'user_{n}')
     password = factory.PostGenerationMethodCall('set_password', 'password')
+    
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+
+    user = factory.SubFactory(UserFactory)
+    name = factory.Faker('name')
+    phone_number = factory.Faker('phone_number')
+    past_address = factory.Faker('address')
+    current_address = factory.Faker('address')
